a55dfc2e06ae6daee8c893ad8933b0bf6ff4dbf3
wuzhangjin1 <wuzhangjin1@oppo.com>
2021-05-11 20:58:38 +0800

    xxx

--- ServiceWorkerTest/service-worker.js
+++ ServiceWorkerTest/service-worker.js
@@ -6,25 +6,24 @@
 // flow and the old cache(s) will be purged as part of the activate event handler when the
 // updated service worker is activated.
 var CACHE_VERSION = 1;
-var PRECACHE = {
+var CURRENT_CACHES  = {
   'read-through': 'read-through-cache-v' + CACHE_VERSION
 };
 
-// A list of local resources we always want to be cached.
-const PRECACHE_URLS = [
-  './styles/main.css'
-];
-
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
-  console.log('Install event:', event);
+  var now = Date.now();
+
+  var urlsToPrefetch = [
+    'styles/main.css'
+  ];
+
+  console.log('Handling install event. Resources to prefetch:', urlsToPrefetch);
+
   event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(
-        // Skip the 'waiting' lifecycle phase, to go directly from 'installed' to 'activated', even if
-        // there are still previous incarnations of this service worker registration active.
-        () => self.skipWaiting())
+    caches.open(CURRENT_CACHES.read-through)
+      .then(cache => cache.addAll(urlsToPrefetch))
+      .then(self.skipWaiting())
   );
 });
   
@@ -34,8 +33,8 @@ self.addEventListener('activate',  event => {
   // Delete all caches that aren't named in CURRENT_CACHES.
   // While there is only one cache in this example, the same logic will handle the case where
   // there are multiple versioned caches.
-  var expectedCacheNames = Object.keys(PRECACHE).map(function(key) {
-    return PRECACHE[key];
+  var expectedCacheNames = Object.keys(CURRENT_CACHES).map(function(key) {
+    return CURRENT_CACHES[key];
   });
 
   event.waitUntil(
@@ -49,9 +48,7 @@ self.addEventListener('activate',  event => {
           }
         })
       );
-    }).then(
-      // Claim any clients immediately, so that the page will be under SW control without reloading.
-      () => self.clients.claim())
+    }).then(self.clients.claim())
   );
 });
   
@@ -62,9 +59,9 @@ self.addEventListener('fetch', function(event) {
   console.log('Handling fetch event for', event.request.url);
 
   event.respondWith(
-    caches.open(PRECACHE['read-through']).then(function(cache) {
+    caches.open(CURRENT_CACHES['read-through']).then(function(cache) {
         return cache.match(event.request).then(function(response) {
-            if (response) {
+          if (response) {
           // If there is an entry in the cache for event.request, then response will be defined
           // and we can just return it.
           console.log(' Found response in cache:', response);
@@ -77,10 +74,7 @@ self.addEventListener('fetch', function(event) {
         console.log(' No response for %s found in cache. ' +
         'About to fetch from network...', event.request.url);
   
-        // We call .clone() on the request since we might use it in the call to cache.put() later on.
-        // Both fetch() and cache.put() "consume" the request, so we need to make a copy.
-        // (see https://fetch.spec.whatwg.org/#dom-request-clone)
-        return fetch(event.request.clone()).then(function(response) {
+        return fetch(event.request).then(function(response) {
             console.log('  Response for %s from network is: %O',
             event.request.url, response);
           // Return the original response object, which will be used to fulfill the resource request.
